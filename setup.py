60d7508a62674d3375514ddaaf3d78e8124ea16c
Filipe De Avila Belbute Peres <filipeabperes@gmail.com>
2017-12-01 12:26:07 -0500

    Updated version

--- setup.py
+++ setup.py
@@ -162,7 +162,7 @@ else:
 
 # Compile the module
 setup(name = "Py3ODE",
-     version = "1.2.0.dev3",
+     version = "1.2.0.dev4",
      description = "Port of PyODE for Python 3",
      author = "see file AUTHORS",
      author_email = "filipeabperes@gmail.com",
